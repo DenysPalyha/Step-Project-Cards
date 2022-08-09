b9cba6a5851415d61bafb3bdca1b93d5a0c14126
Pavlo Nechai <nechaipavel7@gmail.com>
2022-08-09 17:33:03 +0300

    add createCard

---
+++ script/api/createCard.js
@@ -0,0 +1,17 @@
+import instance from "./instance.js"
+
+const createCard = async(body) => {
+    try {
+        const { status, data } = await instance.post('', body);
+        if (status === 200) {
+            console.log(data);
+            return data;
+        } else {
+            console.warn(status, data)
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export default createCard;
