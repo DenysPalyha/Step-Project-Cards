eec9111fed624fef8768bc2c5ecd37544c1620c6
Vladyslav Popil <popilvladislav@gmail.com>
2022-08-10 21:25:10 +0300

    upload_feature_filters

--- script/index.js
+++ script/index.js
@@ -1,6 +1,12 @@
 import Header from "./classes/Header.js";
 import { showAllCards } from "./function/showAllCards.js";
 import { removeBtnLogIngAndAddCreatVisitBtn } from "./function/removeBtnLogingAndAddCreatVisitBtn.js";
+import filterForm from "./layout/filterForm.js";
+import filterCards from "./function/filterCards.js";
+
+filterForm.classList.add("active");
+
+filterCards();
 
 const token = localStorage.getItem("token");
 
